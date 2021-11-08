f2f12661ce063bc71deb8ffcd6be626cbbc6caf8
aliicebraga <aliicebraga.dev@gmail.com>
2021-11-08 02:16:48 -0300

    atualizaçao do footer e links das paginas

--- js/entrar.js
+++ js/entrar.js
@@ -30,12 +30,15 @@ function _cpf(cpf) {
 }
 function validarCPF(el) {
   if (!_cpf(el.value)) {
-    alert('CPF inválido!' + el.value);
+    // alert('CPF inválido!' + el.value);
     // apaga o valor
     el.value = '';
-    document.form1.txCPF.focus();
+    document.formCadastro.cpf.focus();
     return false;
   }
+  // else {
+  //   document.formCadastro.cep.focus();
+  // }
 }
 
 const apresentaDados = resultado => {
@@ -67,86 +70,123 @@ function consultaCEP() {
 
 //Validando os  campos formulário de cadastro
 function validadados() {
+  if (
+    document.formCadastro.nomeCompleto.value == '' ||
+    document.formCadastro.nomeCompleto.value.length < 4
+  ) {
+    alert('Informe um nome válido');
+    //propriedade focus setar para o input nome caso não seja preeenchido
+    document.formCadastro.nomeCompleto.focus();
+    //O Return false evita laço de repetição e perda da prorpiedade focus
+    return false;
+  }
 
-    if (document.formCadastro.nomeCompleto.value == "" || document.formCadastro.nomeCompleto.value.length < 4) {
-        alert("Informe um nome válido");
-        //propriedade focus setar para o input nome caso não seja preeenchido
-        document.formCadastro.nomeCompleto.focus();
-        //O Return false evita laço de repetição e perda da prorpiedade focus
-        return false;
-    }
+  if (
+    document.formCadastro.email.value == '' ||
+    document.formCadastro.email.value.length < 10
+  ) {
+    alert('Informe um E-mail válido');
+    document.formCadastro.email.focus();
+    return false;
+  }
+  if (
+    document.formCadastro.senha.value == '' ||
+    document.formCadastro.senha.value.length < 3
+  ) {
+    alert('Informe uma senha');
+    document.formCadastro.senha.focus();
+    return false;
+  }
 
-    if (document.formCadastro.email.value == "" || document.formCadastro.email.value.length < 10) {
-        alert("Informe um E-mail válido");
-        document.formCadastro.email.focus();
-        return false;
-    }
-    if (document.formCadastro.senha.value == "" || document.formCadastro.senha.value.length < 3) {
-        alert("Informe uma senha");
-        document.formCadastro.senha.focus();
-        return false;
-    }
- 
-     if (document.formCadastro.telefone.value == "" || document.formCadastro.telefone.value.length < 10) {
-        alert("Informe um telefone válido");
-        document.formCadastro.telefone.focus();
-        return false;
-    }
- 
-    if (document.formCadastro.cpf.value == "" || document.formCadastro.cpf.value.length < 5) {
-        alert("Informe um CPF válido");
-        document.formCadastro.cpf.focus();
-        return false;
-    }
+  if (
+    document.formCadastro.telefone.value == '' ||
+    document.formCadastro.telefone.value.length < 10
+  ) {
+    alert('Informe um telefone válido');
+    document.formCadastro.telefone.focus();
+    return false;
+  }
 
-    if (document.formCadastro.cep.value == "" || document.formCadastro.cep.value.length < 7) {
-        alert("Informe um CEP válido");
-        document.formCadastro.cep.focus();
-        return false;
-    }
-    if (document.formCadastro.logradouro.value == "" || document.formCadastro.logradouro.value.length < 5) {
-        alert("Informe um endereço válido");
-        document.form.logradouro.focus();
-        return false;
-    }
-    if (document.formCadastro.bairro.value == "" || document.formCadastro.bairro.value.length < 4) {
-        alert("Informe um bairro válido");
-        document.form.bairro.focus();
-        return false;
-    }
-    if (document.formCadastro.numero.value == "" || document.formCadastro.numero.value.length < 2) {
-        alert("Informe um número válido");
-        document.formCadastro.numero.focus();
-        return false;
-    }
- 
-    if (document.formCadastro.uf.value == "" || document.formCadastro.uf.value.length < 2) {
-        alert("Informe um estado válido");
-        document.formCadastro.uf.focus();
-        return false;
-    }
-    if (document.formCadastro.localidade.value == "" || document.formCadastro.localidade.value.length < 5) {
-        alert("Informe uma cidade válida");
-        document.formCadastro.localidade.focus();
-        return false;
-    }
+  if (
+    document.formCadastro.cpf.value == '' ||
+    document.formCadastro.cpf.value.length < 5
+  ) {
+    alert('Informe um CPF válido');
+    document.formCadastro.cpf.focus();
+    return false;
+  }
+  if (document.formCadastro.cpf.value >= 11) {
+    validarCPF(document.getElementById('cpf').value);
+    document.formCadastro.cep.focus();
+    return false;
+  }
+  // alice testou
 
-    {
-      alert("Cadastro Concluido - Bem Vindo a OpenBook!");
-    }
+  // if (
+  //   document.formCadastro.cep.value == '' ||
+  //   document.formCadastro.cep.value.length < 7
+  // ) {
+  //   alert('Informe um CEP válido');
+  //   document.formCadastro.cep.focus();
+  //   return false;
+  // }
+  if (
+    document.formCadastro.logradouro.value == '' ||
+    document.formCadastro.logradouro.value.length < 5
+  ) {
+    alert('Informe um endereço válido');
+    document.form.logradouro.focus();
+    return false;
+  }
+  if (
+    document.formCadastro.bairro.value == '' ||
+    document.formCadastro.bairro.value.length < 4
+  ) {
+    alert('Informe um bairro válido');
+    document.form.bairro.focus();
+    return false;
+  }
+  if (
+    document.formCadastro.numero.value == '' ||
+    document.formCadastro.numero.value.length < 2
+  ) {
+    alert('Informe um número válido');
+    document.formCadastro.numero.focus();
+    return false;
+  }
+
+  if (
+    document.formCadastro.uf.value == '' ||
+    document.formCadastro.uf.value.length < 2
+  ) {
+    alert('Informe um estado válido');
+    document.formCadastro.uf.focus();
+    return false;
+  }
+  if (
+    document.formCadastro.localidade.value == '' ||
+    document.formCadastro.localidade.value.length < 5
+  ) {
+    alert('Informe uma cidade válida');
+    document.formCadastro.localidade.focus();
+    return false;
+  }
+
+  {
+    alert('Cadastro Concluido - Bem Vindo a OpenBook!');
+  }
 }
 
-
-  //   if (!document.querySelector('[name="rdSexo"]:checked')) {
-  //     alert('Selecione o sexo ao qual vocês se identifica, corretamente!');
-  //     return false;
-  //   }
-  //   if (!document.querySelector('[name="ckFuncao"]:checked')) {
-  //     alert(
-  //       'Selecione as habilidades corespondentes aos seu perfil corretamente!'
-  //     );
-  //     return false;
-  //   }
+//   if (!document.querySelector('[name="rdSexo"]:checked')) {
+//     alert('Selecione o sexo ao qual vocês se identifica, corretamente!');
+//     return false;
+//   }
+//   if (!document.querySelector('[name="ckFuncao"]:checked')) {
+//     alert(
+//       'Selecione as habilidades corespondentes aos seu perfil corretamente!'
+//     );
+//     return false;
+//   }
 
 $('#buttonLogin').click(function () {
   $('#formCadastrar').hide();
@@ -158,4 +198,3 @@ $('#buttonCadastro').click(function () {
   $('#formCadastrar').show();
   document.mensagem.hide;
 });
-
